568ef01d35c997b32741e193ec4062485b9e17cf
silvhua <silvhua@gmail.com>
2024-06-02 22:08:57 -0700

    repair corrupt git objects

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,45 @@
+
+import { useState } from 'react'
+import brainflixLogo from '../../assets/images/BrainFlix-logo.svg'
+import FormField from '../FormField/FormField'
+
+const searchProps = {
+    className: "header__search-input",
+    placeholder: "Search",
+
+}
+
+function SearchInput(props) {
+    return (
+        <>
+        <FormField id="search-input" name="search-input" className={searchProps.className} placeholder={searchProps.placeholder} rows="1"/>
+        </>
+    )
+}
+
+function AvatarDiv() {
+    return (
+        <>
+        <div className="avatar-div header__avatar"></div>
+        </>
+    )
+}
+
+function Header() {
+    return (
+        <>
+        <header>
+            <section className="header">
+                <img src={brainflixLogo} className="header__logo" alt="logo" />
+                <div className="header__search-container">
+                    <SearchInput/>
+                    <AvatarDiv/>
+                    <button className="header__upload-button">Upload</button>
+                </div>
+            </section>
+        </header>
+        </>
+    )
+}
+
+export default Header
