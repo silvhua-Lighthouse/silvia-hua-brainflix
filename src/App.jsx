2acc34ea25c56d7eed568d98aa3a58512ff483f8
silvhua <silvhua@gmail.com>
2024-06-04 07:14:12 -0700

    replace hard-coded article object with videosArray

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,6 @@ import videosArray from './data/video-details.json';
 import CurrentVideo from './components/CurrentVideo/CurrentVideo.jsx';
 import VideoDetails from './components/VideoDetails/VideoDetails.jsx';
 
-const article = {
-  title: "The Future of Artificial Intelligence",
-  author: "Aiden Thompson",
-  date: new Date(2023, 8, 8),
-  views: 980544,
-  likes: 22479,
-  body: "Explore the cutting-edge developments and predictions for Artificial Intelligence in the coming years. From revolutionary breakthroughs in machine learning to the ethical considerations influencing AI advancements, this exploration transcends the boundaries of mere speculation. Join us on a journey that navigates the intricate interplay between innovation, ethics, and the ever-evolving tech frontier."
-}
-
 const video = videosArray[0];
 console.log('App.jsx video', video)
 
@@ -28,7 +19,7 @@ function App() {
     <>
       <Header avatar={avatar}/>
       <main>
-        {/* <CurrentVideo video={video} /> */}
+        <CurrentVideo video={video} />
         <VideoDetails videoDetails={video}></VideoDetails>
         <section>section2</section>
       </main>
