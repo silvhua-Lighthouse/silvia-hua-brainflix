4d764a1da05b3da01f1c4d022e8f1cda5e3c2f8a
silvhua <silvhua@gmail.com>
2024-06-05 14:39:56 -0700

    populate video card details

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,10 +1,14 @@
 import './VideoCard.scss';
 
 const VideoCard = ({videoObject}) => {
-    const {id, title, channel, image} = videoObject;
+    const {title, channel, image} = videoObject;
     return (
         <div className="video-card">
-            <h3 className="video-card__title">{title}</h3>
+            <img src={image} className="video-card__image" />
+            <div className="video-card__text-container">
+                <h3 className="video-card__title">{title}</h3>
+                <p>{channel}</p>
+            </div>
         </div>
     )
 }
