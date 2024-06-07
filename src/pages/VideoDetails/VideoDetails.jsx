f7f304ef63155d66fb8152a2df5346ee1e6e68cf
silvhua <silvhua@gmail.com>
2024-06-07 12:49:48 -0700

    `VideoDetails` shows what is desired

--- src/pages/VideoDetails/VideoDetails.jsx
+++ src/pages/VideoDetails/VideoDetails.jsx
@@ -3,21 +3,27 @@ import './VideoDetails.scss'
 import Description from '../../components/Description/Description.jsx';
 import Metadata from '../../components/Metadata/Metadata.jsx';
 import Comments from '../../components/Comments/Comments.jsx';
+import CurrentVideo from '../../components/CurrentVideo/CurrentVideo.jsx';
 
 function VideoDetails(props) {
     const {avatarSrc, currentVideoObject} =  props;
     const { title, description, comments, ...metadata } = currentVideoObject;
 
     return (
-        <section className="video-details">
-            <article>
-                <h1 className="current-video__title">{title}</h1>
-                <Metadata metadata={metadata}/>
-                <Description description={description}/>
-                <p className="current-video__n-comments">{comments.length} Comments</p>
-            </article>
-        <Comments avatarSrc={avatarSrc} videoObject={currentVideoObject}/>
+        <main>
+        <CurrentVideo currentVideoObject={currentVideoObject} />
+        <section className="secondary">
+            <section className="video-details">
+                <article>
+                    <h1 className="current-video__title">{title}</h1>
+                    <Metadata metadata={metadata}/>
+                    <Description description={description}/>
+                    <p className="current-video__n-comments">{comments.length} Comments</p>
+                </article>
+            <Comments avatarSrc={avatarSrc} videoObject={currentVideoObject}/>
+            </section>
         </section>
+        </main>
     )
 }
 
