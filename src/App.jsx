620935f6784425bb367e1d83f9f5b9e83ca71e8c
silvhua <silvhua@gmail.com>
2024-06-08 09:52:32 -0700

    get default video details

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,30 @@ import './App.scss'
 import Header from './components/Header/Header.jsx'
 import VideoDetails from './pages/VideoDetails/VideoDetails.jsx';
 import Home from './pages/Home/Home.jsx';
+import { useEffect, useState } from 'react';
+import apiInstance from './brainflix-api.js';
 
 function App() {
   const avatarSrc = '../../src/assets/images/Mohan-muruge.jpg';
+  const [videosArray, setVideosArray] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const response = await apiInstance.getVideosArray();
+      setVideosArray(response);
+    }
+    fetchVideos();
+  }, [])
 
   return (
     <BrowserRouter>
       <Header avatarSrc={avatarSrc} classesArray={['header__avatar']} />
       <Routes>
         <Route path="/" element={<Home avatarSrc={avatarSrc} />} />
-        <Route path="videos/:videoId" element={<VideoDetails avatarSrc={avatarSrc} />} />
+        <Route 
+          path="videos/:videoId" 
+          element={<VideoDetails avatarSrc={avatarSrc} videosArray={videosArray}/>} 
+        />
         <Route path="*" element={<Home avatarSrc={avatarSrc} />} />
       </Routes>
     </BrowserRouter>
--- src/pages/VideoDetails/VideoDetails.jsx
+++ src/pages/VideoDetails/VideoDetails.jsx
@@ -10,27 +10,30 @@ import { useEffect, useState } from 'react';
 import apiInstance from '../../brainflix-api.js';
 
 function VideoDetails(props) {
-  const {avatarSrc} =  props;
-  const [videosArray, setVideosArray] = useState([]);
-  const [currentVideoObject, setVideoObject] = useState([]);
-  // let videoObject;
+  const {avatarSrc, videosArray} =  props;
+  const [videoId, setVideoId] = useState(["84e96018-4022-434e-80bf-000ce4cd12b8"]);
+  // const [videoId, setVideoId] = useState([useParams().videoId]);
 
-  useEffect(() => {
-    const fetchVideos = async () => {
-      const response = await apiInstance.getVideosArray();
-      setVideosArray(response);
-      console.log(response)
-    }
-    fetchVideos();
-  }, [])
-  const videoId = useParams().videoId ?? videosArray[0].id;
+  let defaultVideoObject;
+  async function getInitialVideoObject() {
+    defaultVideoObject = await apiInstance.getVideo(videoId);
+  }
+  await getInitialVideoObject();
+  console.log('default video object', defaultVideoObject);
+
+  const [currentVideoObject, setVideoObject] = useState([defaultVideoObject]);
+
+  // console.log('videoId', videoId);
+
+  // const videoId = useParams().videoId ?? videosArray[0].id;
 
   useEffect(() => {
     const fetchVideoObject = async (videoId) => {
-      const response = await apiInstance.getVideo(videoId);
-      setVideoObject(response);
+      const videoDetailsResponse = await apiInstance.getVideo(videoId);
+      setVideoObject(videoDetailsResponse);
+      console.log('current video object', videoDetailsResponse)
     }
-    fetchVideoObject(videoId);
+    setVideoObject(videoId);
   }, []);
   // let currentVideoObject = videosArray[videoId];
 
