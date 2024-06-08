e1b65c8750354185b339cdb79c75840075538414
silvhua <silvhua@gmail.com>
2024-06-08 15:04:38 -0700

    update props passed to FormField()

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.scss'
 import Header from './components/Header/Header.jsx'
 import VideoDetails from './pages/VideoDetails/VideoDetails.jsx';
 import Home from './pages/Home/Home.jsx';
+import VideoUpload from './pages/VideoUpload/VideoUpload.jsx';
 
 function App() {
   const avatarSrc = '../../src/assets/images/Mohan-muruge.jpg';
@@ -17,6 +18,8 @@ function App() {
         <Route 
           path="videos/:videoId" 
           element={<VideoDetails avatarSrc={avatarSrc}/>} 
+        />
+        <Route path="upload" element={<VideoUpload />} 
         />
         <Route path="*" element={<Home avatarSrc={avatarSrc} />} />
       </Routes>
--- src/pages/VideoUpload/VideoUpload.jsx
+++ src/pages/VideoUpload/VideoUpload.jsx
@@ -1,7 +1,25 @@
 import './VideoUpload.scss';
 import previewImage from '../../assets/images/Upload-video-preview.jpg';
+import FormField from '../../components/FormField/FormField';
+import Button from '../../components/Button/Button';
 
 const VideoUpload = () => {
+
+  const inputPropsTitle = {
+    id:"title",
+    name:"title",
+    className:"upload__input-field",
+    placeholder:"Add a title to your video",
+    type:"input",
+    rows:"1"
+  }
+  const inputPropsDescription = {id:"description",
+    name:"description",
+    className:"upload__input-field",
+    placeholder:"Add a description to your video",
+    type:"textarea",
+    rows:"4"
+  }
   return (
     <section>
       <h1 className="upload__title">Upload Video</h1>
@@ -14,32 +32,18 @@ const VideoUpload = () => {
         />
       </div>
       <form className="upload__form">
-        <FormField 
-          id="title" 
-          name="title" 
-          className="upload__input-field" 
-          placeholder="Add a title to your video" 
-          type="input" 
-          rows="1"
-        />
-        <FormField 
-          id="description" 
-          name="description" 
-          className="upload__input-field" 
-          placeholder="Add a description to your video" 
-          type="textarea" 
-          rows="4"
-        />
+        <FormField inputProps={inputPropsTitle} />
+        <FormField inputProps={inputPropsDescription} />
       </form>
       <div className="upload__buttons-container">
-        <Button 
+        {/* <Button 
           className="upload__button--cancel" 
           type="reset" 
         />
         <Button 
           className="upload__button--publish" 
           type="submit"
-        />
+        /> */}
       </div>
     </section>
   )
