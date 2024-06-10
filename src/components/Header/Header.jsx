d10b1ef2cd2056d677e3340b7366797f773009c1
silvhua <silvhua@gmail.com>
2024-06-09 17:51:47 -0700

    link header logo to home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
-import brainflixLogo from '../../assets/images/BrainFlix-logo.svg'
-import FormField from '../FormField/FormField'
-import './Header.scss'
-import AvatarDiv from '../Avatar/Avatar'
-import Button from '../Button/Button'
+import brainflixLogo from '../../assets/images/BrainFlix-logo.svg';
+import FormField from '../FormField/FormField';
+import './Header.scss';
+import AvatarDiv from '../Avatar/Avatar';
+import Button from '../Button/Button';
+import { Link } from 'react-router-dom';
 
 function Header(props) {
     const avatarProps = props;
@@ -25,7 +26,9 @@ function Header(props) {
         <>
         <header>
             <nav className="header">
-                <img src={brainflixLogo} className="header__logo" alt="logo" />
+                <Link to="/"  className="header__logo">
+                    <img src={brainflixLogo} alt="logo" />
+                </Link>
                 <div className="header__nav-container">
                     <FormField inputProps={searchProps}/>
                     <AvatarDiv avatar={avatarProps}/>
