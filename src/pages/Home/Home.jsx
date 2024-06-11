e8ca616065c8c9501698feb4788be824f3bc9159
silvhua <silvhua@gmail.com>
2024-06-10 21:24:29 -0700

    remove `Home()`; rename `VideoDetails()` to `Home()`

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,41 @@
-import './Home.scss';
-import VideoDetails from '../VideoDetails/VideoDetails';
+import { useParams } from 'react-router-dom';
+import './Home.scss'
+import CurrentVideo from '../../components/CurrentVideo/CurrentVideo.jsx';
+import { useEffect, useState } from 'react';
+import apiInstance from '../../brainflix-api.js';
 
-const Home = ({avatarSrc, videosArray, defaultId}) => {
-  
+function VideoDetails(props) {
+  const {avatarSrc} =  props;
+  const [videosArray, setVideosArray] = useState([]);
+  const [videoId, setVideoId] = useState(useParams().videoId);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const response = await apiInstance.getVideosArray();
+      setVideosArray(response);
+      if (!videoId) {
+        setVideoId(response[0].id);
+      }
+    }
+    fetchVideos();
+
+  }, []);
+
+  if (videosArray.length === 0) {
+    return (
+      <main>Loading...</main>
+    );
+  }
+
   return (
-    <>
-    <VideoDetails 
+    <main>
+      <CurrentVideo 
+      // videoId={videoId} 
+      videosArray={videosArray} 
       avatarSrc={avatarSrc} 
-      // videosArray={videosArray} 
-      // defaultId={defaultId}
       />
-    </>
+    </main>
   )
 }
 
-export default Home
+export default VideoDetails;
